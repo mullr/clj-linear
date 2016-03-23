c9b89dfae5ca735dea275b4b98220c3d6506d94a
Russell Mull <Russell Mull russell@digitalartificer.com>
2016-03-22 21:09:15 -0700

    Update project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-linear "0.1.0-SNAPSHOT"
   :description "A nice clojure interface for doing linear programming."
-  :url "http://example.com/FIXME"
+  :url "http://github.com/mullr/clj-linear"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
