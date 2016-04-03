e43d39aa393ed160b42916439f498981aef53cce
Russell Mull <russell@digitalartificer.com>
2016-04-03 00:20:00 -0700

    Update to commons-math 3.6.1

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.commons/commons-math3 "3.2"]])
+                 [org.apache.commons/commons-math3 "3.6.1"]])
